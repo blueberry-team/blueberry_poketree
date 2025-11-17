8e7468506dcece777484f55a3be7f599bf248ffa
Lana <lanafksk@naver.com>
2025-11-18 01:23:31 +0900

    feat: 기본 규격 사이즈 container 생성

---
+++ features/shared/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      {/* 데스크탑일 때만 보이는 빨간색 외부 박스 - 컨텐츠보다 살짝 크게 */}
+      <div className="hidden md:block absolute w-[calc(390px+8rem)] h-[calc(845px+5rem)] bg-[#DC0A2D]"></div>
+
+      {/* 실제 컨텐츠 컨테이너 - 모바일 뷰 사이즈 고정 */}
+      <div className="w-full md:w-[390px] h-screen md:h-[845px] flex flex-col overflow-hidden relative z-10">
+        {children}
+      </div>
+    </div>
+  );
+}
+
