d4a5c8d19550324a0bb4168313daece8d6647e57
dusagong <yoonseungjae123@gmail.com>
2025-11-18 23:23:46 +0900

    fix: my-tree와 my-poket-message 페이지 원래 상태로 복원

--- app/my-poket-message/page.tsx
+++ app/my-poket-message/page.tsx
@@ -1,37 +1,9 @@
-"use client";
-
-import { BottomButtons } from "@/features/shared/components/BottomButtons/BottomButtons";
-import { useTranslation } from "@/features/shared/hooks/useTranslation";
-
 // 내 포켓 메시지 페이지
 
 export default function MyPoketMessagePage() {
-  const { t } = useTranslation();
-  const handleUp = () => {
-    console.log("이전 메시지");
-  };
-
-  const handleDown = () => {
-    console.log("다음 메시지");
-  };
-
-  const handleLeft = () => {
-    console.log("메시지 보관");
-  };
-
-  const handleRight = () => {
-    console.log("메시지 삭제");
-  };
-
-  return (
-    <>
-      <h1>{t("message.title")}</h1>
-      <BottomButtons
-        onUp={handleUp}
-        onDown={handleDown}
-        onLeft={handleLeft}
-        onRight={handleRight}
-      />
-    </>
-  );
+    return (
+        <div>
+            <h1>My Poket Message</h1>
+        </div>
+    );
 }
--- app/my-tree/page.tsx
+++ app/my-tree/page.tsx
@@ -1,37 +1,9 @@
-"use client";
-
-import { BottomButtons } from "@/features/shared/components/BottomButtons/BottomButtons";
-import { useTranslation } from "@/features/shared/hooks/useTranslation";
-
 // 내 트리 페이지
 
 export default function MyTreePage() {
-  const { t } = useTranslation();
-  const handleUp = () => {
-    console.log("트리 확대");
-  };
-
-  const handleDown = () => {
-    console.log("트리 축소");
-  };
-
-  const handleLeft = () => {
-    console.log("이전 트리");
-  };
-
-  const handleRight = () => {
-    console.log("다음 트리");
-  };
-
-  return (
-    <>
-      <h1>{t("tree.title")}</h1>
-      <BottomButtons
-        onUp={handleUp}
-        onDown={handleDown}
-        onLeft={handleLeft}
-        onRight={handleRight}
-      />
-    </>
-  );
+    return (
+        <div>
+            <h1>My Tree</h1>
+        </div>
+    );
 }
