c8a9fe5fad588b4f6cb8312e9de65f9cbff206d9
dusagong <yoonseungjae123@gmail.com>
2025-11-18 16:55:32 +0900

    fix: 타이포 수정

--- app/my-poket-message/page.tsx
+++ app/my-poket-message/page.tsx
@@ -1,9 +1,9 @@
 // 내 포켓 메시지 페이지
 
-export default function MyTreePage() {
+export default function MyPoketMessagePage() {
     return (
         <div>
-            <h1>My Tree</h1>
+            <h1>My Poket Message</h1>
         </div>
     );
 }
