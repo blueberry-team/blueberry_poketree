c65471230a9564ce03b465084d9eab84879afbc1
dusagong <yoonseungjae123@gmail.com>
2025-11-29 21:07:17 +0900

    fix: 컨테이너색 복원

--- features/shared/components/Container/Container.tsx
+++ features/shared/components/Container/Container.tsx
@@ -5,7 +5,7 @@ export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex items-center justify-center">
       {/* 데스크탑일 때만 보이는 빨간색 외부 박스 - 컨텐츠보다 살짝 크게 */}
-      <div className="hidden md:block absolute w-[calc(390px+2rem)] h-[calc(780px+2rem)] bg-[#BFd120]"></div>
+      <div className="hidden md:block absolute w-[calc(390px+2rem)] h-[calc(780px+2rem)] bg-[#BF0120]"></div>
 
       {/* 실제 컨텐츠 컨테이너 - 모바일 뷰 사이즈 고정 */}
       <div className="w-full md:w-[390px] h-screen md:h-[780px] flex flex-col overflow-y-auto scrollbar-hide relative z-10 bg-[#DC0A2D]">
