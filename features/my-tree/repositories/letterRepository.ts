6718830b4cbd492a78864e90e3bc295b5f6d8d5c
Lana <lanafksk@naver.com>
2025-11-30 15:43:50 +0900

    fix: get letter 기능 활성화

--- features/my-tree/repositories/letterRepository.ts
+++ features/my-tree/repositories/letterRepository.ts
@@ -12,28 +12,10 @@ import type { DeleteLetterRequest } from "../models/req/DeleteLetterRequest";
 export async function getLetterById(
   req: GetLetterRequest
 ): Promise<GetLetterResponse> {
-  // TODO: 서버 구현 후 주석 해제
-  // return apiClient.get<GetLetterResponse>(
-  //   `/letter/${req.letter_id}`,
-  //   true,  // 인증 필요
-  //   { 'letter-id': req.letter_id }  // 헤더에 letter_id 포함
-  // );
-
-  // 샘플 데이터 반환
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        message: "success",
-        data: {
-          letter_id: req.letter_id,
-          sender_name: "아구몬",
-          is_open: true,
-          letter_pokemon: 49,
-          content: "디지몬이 짱임",
-        },
-      });
-    }, 300);
-  });
+  return apiClient.get<GetLetterResponse>(
+    `/letter/${req.letter_id}`,
+    true,  // 인증 필요
+  );
 }
 
 /**
