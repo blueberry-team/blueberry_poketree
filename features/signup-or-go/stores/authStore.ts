7bf8d35db1a4a05f90ce6c3cbe6381a5e39ebac9
Lana <lanafksk@naver.com>
2025-11-30 13:20:45 +0900

    fix: userId 의 경우 session storage 사용해서 저장하게 변경

--- features/signup-or-go/stores/authStore.ts
+++ features/signup-or-go/stores/authStore.ts
@@ -2,14 +2,15 @@ import { signal } from "@preact/signals-react";
 
 // 로그인 상태를 관리하는 signal
 export const isLoggedIn = signal<boolean>(false);
-export const userId = signal<string>('');
+export const userId = signal<string>("");
 
 // 초기화 함수 (클라이언트 사이드에서만 실행)
 export function initAuthState() {
   if (typeof window !== "undefined") {
     const token = sessionStorage.getItem("accessToken");
+    const storedUserId = sessionStorage.getItem("userId");
     isLoggedIn.value = !!token;
-    userId.value = ''
+    userId.value = storedUserId || "";
   }
 }
 
@@ -21,4 +22,11 @@ export function setLoggedIn(value: boolean) {
 // 유저 ID 설정
 export function setUserId(value: string) {
   userId.value = value;
+  if (typeof window !== "undefined") {
+    if (value) {
+      sessionStorage.setItem("userId", value);
+    } else {
+      sessionStorage.removeItem("userId");
+    }
+  }
 }
