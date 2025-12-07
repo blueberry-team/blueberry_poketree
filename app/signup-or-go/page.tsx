2a0631c16514e36a689e7597632ecb418822f314
Lana <lanafksk@naver.com>
2025-12-07 11:45:16 +0900

    fix: 로그인 페이지에서 헤더 움직이는 이슈 해결

--- app/signup-or-go/page.tsx
+++ app/signup-or-go/page.tsx
@@ -60,7 +60,7 @@ function SignupOrGoPageContent() {
     };
 
     return (
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col min-h-0">
             <AuthForm
                 onSubmit={handleSignupOrGo}
                 isLoading={isLoading}
