d034ff3ab75b8ea9780cf75336a4c46b00c95488
Lana <lanafksk@naver.com>
2025-12-07 15:52:28 +0900

    fix: cross button size 수정

--- features/shared/components/BottomButtons/BottomButtons.tsx
+++ features/shared/components/BottomButtons/BottomButtons.tsx
@@ -47,7 +47,7 @@ function BottomButtonsContent({
   const buttonLabel = translate("pokedex.button");
 
   return (
-    <div className="flex justify-between items-end">
+    <div className="flex justify-between items-end gap-4">
       {/* 왼쪽: 메시지 버튼 + 도감 버튼 (세로 배치) */}
       <div className="flex flex-col gap-4">
         {/* 메시지 확인 버튼 (onCheckMessage가 전달된 경우) */}
@@ -58,7 +58,7 @@ function BottomButtonsContent({
               onCheckMessage();
             }}
             className="relative flex items-center justify-center"
-            style={{ width: "193px", height: "56px" }}
+            style={{ width: "180px", height: "56px" }}
           >
             <Image
               src={ButtonMediumDark}
@@ -80,7 +80,7 @@ function BottomButtonsContent({
               onSendMessage();
             }}
             className="relative flex items-center justify-center"
-            style={{ width: "193px", height: "56px" }}
+            style={{ width: "180px", height: "56px" }}
           >
             <Image
               src={ButtonMediumDark}
@@ -98,7 +98,7 @@ function BottomButtonsContent({
         <button
           onClick={handleButtonClick}
           className="relative flex items-center justify-center"
-          style={{ width: "193px", height: "56px" }}
+          style={{ width: "180px", height: "56px" }}
         >
           <Image
             src={ButtonMediumSkyblue}
